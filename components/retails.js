f3220caefdfe618b01d9c18e353982d067b52998
Simon R <nexus-uw@users.noreply.github.com>
2017-05-23 22:05:29 -0400

    fixed logo display

---
+++ components/retails.js
@@ -0,0 +1,52 @@
+function shuffle(input) {
+  console.log('shuffle')
+
+  const array = [].concat(input);
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+  // // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+export const list = shuffle([
+  {
+    name: 'Cabela\'s (all over Canada)',
+    link: 'http://www.cabelas.ca',
+    img: require('~assets/cabelas-logo.png'),
+    id: 1
+  }, {
+    name: 'Sail (ON + QC)',
+    link: 'https://www.sail.ca/',
+    img: require('~assets/sail-logo.png'),
+    id: 2
+  }, {
+    name: 'Firearms Outlet Canada (Ajax, ON)',
+    link: 'http://www.firearmsoutletcanada.com',
+    img: require('~assets/foc-logo.jpg'),
+    id: 3
+  }, {
+    name: 'Al Flaherty\'s (Toronto, ON)',
+    link: 'https://www.alflahertys.com',
+    img: require('~assets/al-flahertys-logo.png'),
+    id: 4
+  }, {
+    name: 'Bulls Eye London (London. ON)',
+    link: 'http://www.bullseyelondon.com/',
+    img: require('~assets/bulls-logo.png'),
+    id: 5
+  }
+]);
