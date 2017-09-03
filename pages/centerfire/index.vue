66b0ddb10ecbaaa0dc6545d5835e4175e45430aa
Simon Ramsay <nexus-uw@users.noreply.github.com>
2017-09-02 22:58:00 -0700

    save page + calibre in url (#79)
    - moved values entierly into page components
    - get + set page in url just like calibre

--- pages/centerfire/index.vue
+++ pages/centerfire/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container">
     <h1>Centerfire</h1>
-    <my-table v-if="!error" v-bind:rows="rows" v-bind:calibre="calibre"></my-table>
+    <my-table v-if="!error" v-bind:rows="rows" :calibre.sync="calibre" :page.sync="page"></my-table>
     <div v-if="error">failed to load ammo list.</div>
   </div>
 </template>
 
 <script>
 import MyTable from '~/components/my-table.vue'
+
+function updateUrl(page, calibre) {
+  history.pushState({}, 'Best-centerfire-prices', window.location.pathname + `?calibre=${encodeURIComponent(calibre)}&page=${page}`);
+}
 
 export default {
   head: {
@@ -23,13 +27,24 @@ export default {
     return {
       error: null,
       rows: [],
-      calibre: ''
+      calibre: '',
+      page: 1
+    }
+  },
+  watch: {
+    page: function() {
+      updateUrl(this.page, this.calibre)
+    },
+    calibre: function() {
+      updateUrl(this.page, this.calibre)
     }
   },
   async asyncData({ error, query, app }) {
     try {
       let res = await app.$axios.get(BASE_API_URL + 'centerfire');
-      return { rows: res.data, calibre: query.calibre || '' };
+      const rows = res.data;
+      const page = parseInt(query.page || '1');
+      return { rows, calibre: query.calibre || '', page };
     } catch (e) {
       console.error(e);
       return { statusCode: 500, message: 'Failed to load prices', error: true };
