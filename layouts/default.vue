ae77276bf0e5429f2928a5a67d3c6265bf4ffeb9
Simon Ramsay <nexus-uw@users.noreply.github.com>
2017-11-26 13:17:38 -0500

    created social links in footer (#114)

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,6 +46,12 @@
     <footer class="footer">
       AmmoBin.ca {{new Date().getFullYear()}}
       <a v-bind:href="mail">Contact</a>
+      <div>
+        <a href="https://github.com/ammobindotca" target="_blank" rel="noopener">github</a> |
+        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a> |
+        <a href="https://twitter.com/ammobinca" target="_blank" rel="noopener">twitter</a> |
+        <a href="https://www.facebook.com/ammobinca-329189654220485" target="_blank" rel="noopener">facebook</a>
+      </div>
     </footer>
   </div>
 </template>
