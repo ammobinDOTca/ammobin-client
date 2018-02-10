beb419adcd24138dc3f89640bd2f26f67f7c6922
Simon Ramsay <nexus-uw@users.noreply.github.com>
2018-02-10 12:41:44 -0700

    show all content to w3m users

--- helpers.js
+++ helpers.js
@@ -22,5 +22,8 @@ export function updateUrl(title, page, calibre) {
 export function isCrawler(req) {
   const userAgent = (req ? req.headers['user-agent'] : (typeof navigator !== 'undefined' ? navigator.userAgent : 'No user agent (generated)'));
 
-  return ['Googlebot', 'Bingbot', 'Slurp', 'DuckDuckBot','Yandex'].some(bot => userAgent.indexOf(bot) >= 0);
+  return [
+    'Googlebot', 'Bingbot', 'Slurp', 'DuckDuckBot', 'Yandex',
+    'w3m' // let elite haxors see all content b/c they are too cool for JS
+  ].some(bot => userAgent.indexOf(bot) >= 0);
 }
