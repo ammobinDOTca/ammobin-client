513cfd862f610e7686ab6ef70b01fa1d7ddfae66
Simon R <nexus-uw@users.noreply.github.com>
2018-02-17 12:40:31 -0500

    added north pro sports + updated readme

--- components/retails.js
+++ components/retails.js
@@ -23,177 +23,183 @@ function shuffle(input) {
 export const list = shuffle([
   {
     name: 'Cabela\'s (all over Canada)',
-    link: 'http://www.cabelas.ca/?utm_source=ammobin.ca',
+    link: 'http://www.cabelas.ca/',
     img: require('~/assets/cabelas-logo.png'),
-    id: 1
+
   }, {
     name: 'Sail (ON + QC)',
-    link: 'https://www.sail.ca/?utm_source=ammobin.ca',
+    link: 'https://www.sail.ca/',
     img: require('~/assets/sail-logo.png'),
-    id: 2
+
   }, {
     name: 'Firearms Outlet Canada (Ajax, ON)',
-    link: 'http://www.firearmsoutletcanada.com/?utm_source=ammobin.ca',
+    link: 'http://www.firearmsoutletcanada.com/',
     img: require('~/assets/foc-logo.jpg'),
-    id: 3
+
   }, {
     name: 'Al Flaherty\'s (Toronto, ON)',
-    link: 'https://www.alflahertys.com/?utm_source=ammobin.ca',
+    link: 'https://www.alflahertys.com/',
     img: require('~/assets/al-flahertys-logo.png'),
-    id: 4
+
   }, {
     name: 'Bulls Eye London (London. ON)',
-    link: 'http://www.bullseyelondon.com/?utm_source=ammobin.ca',
+    link: 'http://www.bullseyelondon.com/',
     img: require('~/assets/bulls-logo.png'),
-    id: 5
+
   }, {
     name: 'Canadian Tire (all over Canada)',
-    link: 'http://www.canadiantire.ca/en/sports-rec/hunting/ammunition.html?utm_source=ammobin.ca',
+    link: 'http://www.canadiantire.ca/en/sports-rec/hunting/ammunition.html',
     img: require('~/assets/canadian-tire-logo.png'),
-    id: 6
+
   }, {
     name: 'Reliable Gun (Vancouver, BC)',
-    link: 'https://www.reliablegun.com/?utm_source=ammobin.ca',
+    link: 'https://www.reliablegun.com/',
     img: require('~/assets/reliable-gun-logo.jpg'),
-    id: 7
+
   }, {
     name: 'Tenda (Richmond Hill, ON)',
-    link: 'https://www.gotenda.com/?utm_source=ammobin.ca',
+    link: 'https://www.gotenda.com/',
     img: require('~/assets/tenda-logo.png'),
-    id: 8
+
   }, {
     name: 'Canada Ammo  (BC and ON)',
-    link: 'https://www.canadaammo.com/?utm_source=ammobin.ca',
+    link: 'https://www.canadaammo.com/',
     img: require('~/assets/canada-ammo-logo.jpg'),
-    id: 9
+
   }, {
     name: 'Wolverine Supplies (Virden, MB)',
-    link: 'https://www.wolverinesupplies.com/?utm_source=ammobin.ca',
+    link: 'https://www.wolverinesupplies.com/',
     img: require('~/assets/wolverine-logo.png'),
-    id: 10
+
   }, {
     name: 'Jo Brook Outdoors (Brandon, MB)',
-    link: 'https://www.jobrookoutdoors.com/?utm_source=ammobin.ca',
+    link: 'https://www.jobrookoutdoors.com/',
     img: require('~/assets/jo-brook-logo.png'),
-    id: 11
+
   }, {
     name: 'The Ammo Source (Harrowsmith, ON)',
-    link: 'http://www.theammosource.com/?utm_source=ammobin.ca',
+    link: 'http://www.theammosource.com/',
     img: require('~/assets/the-ammo-source-logo.png'),
-    id: 12
+
   }, {
     name: 'Hirsch Precision (Lake Echo, NS)',
-    link: 'http://www.hirschprecision.com/?utm_source=ammobin.ca',
+    link: 'http://www.hirschprecision.com/',
     img: require('~/assets/hirsch-logo.png'),
-    id: 13
+
   }, {
     name: 'Wild West (Edmonton, AB)',
-    link: 'https://gun-shop.ca/?utm_source=ammobin.ca',
+    link: 'https://gun-shop.ca/',
     img: require('~/assets/wild.png'),
-    id: 14
+
   }, {
     name: 'Tiger Arms (Port Coquitlam, BC)',
-    link: 'http://www.tigerarms.ca/?utm_source=ammobin.ca',
+    link: 'http://www.tigerarms.ca/',
     img: require('~/assets/tigerarms.png'),
-    id: 15,
+
     background: true,
   }, {
     name: 'Mag Dump (Sherwood Park, AB)',
-    link: 'https://magdump.ca/?utm_source=ammobin.ca',
+    link: 'https://magdump.ca/',
     img: require('~/assets/magdump-logo.png'),
-    id: 16
+
   },
   {
     name: 'Rangeview Sports (Newmarket, ON)',
-    link: 'https://www.rangeviewsports.ca/?utm_source=ammobin.ca',
+    link: 'https://www.rangeviewsports.ca/',
     img: require('~/assets/rangeview-sports.png'),
-    id: 17
   },
   {
-
     name: 'Trade Ex Canada (Hawkesbury, ON)',
-    link: 'https://www.tradeexcanada.com/?utm_source=ammobin.ca',
+    link: 'https://www.tradeexcanada.com/',
     img: require('~/assets/tradex-logo.png'),
-    id: 18
   }, {
     name: 'Frontier Firearms (Prince Albert, SK)',
-    link: 'http://frontierfirearms.ca/?utm_source=ammobin.ca',
+    link: 'http://frontierfirearms.ca/',
     img: require('~/assets/frontierfirearms-logo.png'),
-    id: 19
+
   }, {
     name: 'BV Outdoor Essentials (Smithers, BC)',
-    link: 'https://www.bvoutdoors.com/?utm_source=ammobin.ca',
+    link: 'https://www.bvoutdoors.com/',
     img: require('~/assets/bvoutdoors-logo.png'),
-    id: 20
+
   }, {
     name: 'NAS Guns & Ammo (Niagara and Sault Ste. Marie, ON)',
-    link: 'https://www.nasgunsandammo.com/?utm_source=ammobin.ca',
+    link: 'https://www.nasgunsandammo.com/',
     img: require('~/assets/nas-logo.jpg'),
-    id: 21,
+
   }, {
     name: 'Dante Sports (Montréal QC)',
-    link: 'https://www.dantesports.com/en/?utm_source=ammobin.ca',
+    link: 'https://www.dantesports.com/en/',
     img: require('~/assets/dante-logo.png'),
-    id: 22,
+
   },
   {
     name: 'Lever Arms (Vancouver, BC)',
-    link: 'https://www.leverarms.com/?utm_source=ammobin.ca',
+    link: 'https://www.leverarms.com/',
     img: require('~/assets/leverarms-logo.png'),
-    id: 23
+
   },
   {
     name: 'Calgary Shooting Center (Calgary, AB)',
-    link: 'https://store.theshootingcentre.com/?utm_source=ammobin.ca',
+    link: 'https://store.theshootingcentre.com/',
     img: require('~/assets/shooting-center-logo.jpg'),
-    id: 24
+
   },
   {
     name: 'Western Metal (AB)',
-    link: 'https://www.westernmetal.ca/shooting?utm_source=ammobin.ca',
+    link: 'https://www.westernmetal.ca/shooting',
     img: require('~/assets/westernmetal-logo.png'),
-    id: 25
+
   },
   {
     name: 'Al Simmons (Hamilton, ON)',
-    link: 'https://alsimmonsgunshop.com/?utm_source=ammobin.ca',
+    link: 'https://alsimmonsgunshop.com/',
     img: require('~/assets/al-simmons-logo.jpg'),
-    id: 26
+
   },
   {
     name: 'Vancouver Gun Store (Vancouver, BC)',
-    link: 'https://vancouvergunstore.ca/?utm_source=ammobin.ca',
+    link: 'https://vancouvergunstore.ca/',
     img: require('~/assets/vancouvergunstore-logo.png'),
-    id: 27
+
   },
   {
     name: 'Bartons Big Country (Grande Prairie, AB)',
-    link: 'https://www.bartonsbigcountry.ca/?utm_source=ammobin.ca',
+    link: 'https://www.bartonsbigcountry.ca/',
     img: require('~/assets/logo-bartons.png'),
-    id: 28
+
   },
   {
     name: 'The Shooting Edge (Calgary, AB)',
-    link: 'https://theshootingedge.com/?utm_source=ammobin.ca',
+    link: 'https://theshootingedge.com/',
     img: require('~/assets/shootingedge-logo.png'),
-    id: 29
+
   },
   {
     name: 'Lanz Shooting Supplies (St Ann, ON)',
-    link: 'http://www.lanzshootingsupplies.com/?utm_source=ammobin.ca',
+    link: 'http://www.lanzshootingsupplies.com/',
     img: require('~/assets/lanz-logo.png'),
-    id: 30
+
   },
   {
     name: 'Duram Outdoors (Orono, ON)',
-    link: 'https://durhamoutdoors.ca/?utm_source=ammobin.ca',
+    link: 'https://durhamoutdoors.ca/',
     img: require('~/assets/duram-logo.png'),
-    id: 31
+
   },
   {
     name: 'Soley Outdoors (Markham, ON)',
-    link: 'https://www.solelyoutdoors.com/?utm_source=ammobin.ca',
+    link: 'https://www.solelyoutdoors.com/',
     img: require('~/assets/soley-logo.png'),
-    id: 32
   },
-]);
+  {
+    name: 'North Pro Sports (Saskatoon, SK)',
+    link: 'http://northprosports.com/',
+    img: require('~/assets/northpro-logo.png'),
+    background: true
+  },
+].map((i, index) => {
+  i.link += '?utm_source=ammobin.ca'
+  i.id = index;
+  return i;
+}));
