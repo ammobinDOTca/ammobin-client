d52234aa2f97132e96a9dbfab163172281cc32df
Simon Ramsay <nexus-uw@users.noreply.github.com>
2018-02-27 14:08:01 -0500

    generate site map for ALL calibres

---
+++ generate-sitemap.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const { head } = require('ramda')
+const ws = fs.createWriteStream('./static/sitemap.txt')
+const site = 'https://ammobin.ca/';
+
+
+['', 'about', 'dank', 'rimfire', 'shotgun', 'centerfire'].forEach(s => ws.write(`${site}${s}\n`))
+
+require('ammobin-classifier/build/rimfire-calibres').rimfireCalibres
+  .map(l => head(l).toUpperCase())
+  .forEach(c => ws.write(`${site}rimfire?calibre=${encodeURIComponent(c)}&page=1\n`))
+
+require('ammobin-classifier/build/centerfire-calibres').centerFireCalibres
+  .map(l => head(l).toUpperCase())
+  .forEach(c => ws.write(`${site}centerfire?calibre=${encodeURIComponent(c)}&page=1\n`))
+
+require('ammobin-classifier/build/shotgun-gauges').shotgunGauges
+  .map(l => head(l).toUpperCase())
+  .forEach(c => ws.write(`${site}shotgun?calibre=${encodeURIComponent(c)}&page=1\n`))
+
+
