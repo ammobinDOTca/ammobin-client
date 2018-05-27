2293de4e22bb0074b6231691f0657fdca27420ec
Simon R <nexus-uw@users.noreply.github.com>
2018-05-27 13:15:58 -0700

    added link to status page

--- layouts/default.vue
+++ layouts/default.vue
@@ -48,7 +48,9 @@
       <a v-bind:href="mail">Contact</a>
       <div>
         <a href="https://github.com/ammobindotca" target="_blank" rel="noopener">github</a> |
-        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a> 
+        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a> |
+        <a href="https://status.ammobin.ca/" target="_blank"
+        rel="nooperner">status</a>
       </div>
     </footer>
   </div>
