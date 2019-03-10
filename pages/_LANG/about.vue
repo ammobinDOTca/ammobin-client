0b1e2e611710206f11bf8475b9432fd19e351af7
Simon R <nexus-uw@users.noreply.github.com>
2019-03-10 15:40:13 -0700

    removed link to gunsale.ca since that site appears to no longer exist
    - formatted file
    - please let me know if it comes back and I will re-enable it

--- pages/_LANG/about.vue
+++ pages/_LANG/about.vue
@@ -1,69 +1,66 @@
 <template>
   <div class="container">
-    <img src="~/assets/logo-medium.png" class="pure-img img" alt="ammobin.ca logo">
-    <h1>{{$t('default.about')}}</h1>
+    <img src="~/assets/logo-medium.png" class="pure-img img" alt="ammobin.ca logo" />
+    <h1>{{ $t('default.about') }}</h1>
     <div>
       <a href="/">ammobin.ca</a>
-      {{$t('about.main1')}}
+      {{ $t('about.main1') }}
     </div>
-    <div>{{$t('about.main2')}}</div>
-    <div>{{$t('about.main3')}}</div>
+    <div>{{ $t('about.main2') }}</div>
+    <div>{{ $t('about.main3') }}</div>
 
-    <hr>
+    <hr />
 
-    <h2>{{$t('about.contactInfo')}}</h2>
+    <h2>{{ $t('about.contactInfo') }}</h2>
     <div>
-      {{$t('about.contactQuestions1')}}
-      <div>{{$t('about.contactQuestion2')}}</div>
-      <div>{{$t('about.contactQuestion3')}}</div>
-      <div>{{$t('about.contactQuestion4')}}</div>
+      {{ $t('about.contactQuestions1') }}
+      <div>{{ $t('about.contactQuestion2') }}</div>
+      <div>{{ $t('about.contactQuestion3') }}</div>
+      <div>{{ $t('about.contactQuestion4') }}</div>
 
       <div style="margin-top:10px;">
-        {{$t('about.submitIssue')}}
-        <a
-          href="https://github.com/ammobinDOTca"
-          target="_blank"
-          rel="noopener"
-        >https://github.com/ammobindotca</a>
+        {{ $t('about.submitIssue') }}
+        <a href="https://github.com/ammobinDOTca" target="_blank" rel="noopener">https://github.com/ammobindotca</a>
       </div>
       <div>
-        {{$t('about.sendEmail')}}
-        <a v-bind:href="mail">{{email}}</a>
+        {{ $t('about.sendEmail') }}
+        <a v-bind:href="mail">{{ email }}</a>
       </div>
     </div>
 
-    <hr>
+    <hr />
 
-    <h2>{{$t('about.disclaimerTitle')}}</h2>
-    <p>{{$t('about.disclaimer')}}</p>
+    <h2>{{ $t('about.disclaimerTitle') }}</h2>
+    <p>{{ $t('about.disclaimer') }}</p>
 
-    <hr>
+    <hr />
 
-    <h2>{{$t('about.incorrectCountTitle')}}</h2>
-    <p>{{$t('about.incorrectCountText')}}</p>
+    <h2>{{ $t('about.incorrectCountTitle') }}</h2>
+    <p>{{ $t('about.incorrectCountText') }}</p>
 
-    <hr>
+    <hr />
 
-    <h2 id="supportedRetailers">{{$t('about.supportedRetailers')}}</h2>
-<div class="pure-g">
-    <div v-if="vendors" class="pure-u-1 pure-u-md-1-3 m-t-2 m-r-2" v-for="row in randomVendors" :key="row.link">
-      <a v-bind:href="row.link" target="_blank" rel="noopener">
-        <img
-          v-bind:src="row.logo"
-          class="pure-img img"
-          v-bind:class="{'grey-background':row.background}"
-          v-bind:alt="row.name"
-          v-bind:title="row.name"
-        >
-      </a>
+    <h2 id="supportedRetailers">{{ $t('about.supportedRetailers') }}</h2>
+    <div class="pure-g">
+      <div v-if="vendors" class="pure-u-1 pure-u-md-1-3 m-t-2 m-r-2" v-for="row in randomVendors" :key="row.link">
+        <a v-bind:href="row.link" target="_blank" rel="noopener">
+          <img
+            v-bind:src="row.logo"
+            class="pure-img img"
+            v-bind:class="{ 'grey-background': row.background }"
+            v-bind:alt="row.name"
+            v-bind:title="row.name"
+          />
+        </a>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-3 m-t-2">
+        <h4>{{ $t('about.moreRetailers') }}</h4>
+      </div>
     </div>
-    <div class="pure-u-1 pure-u-md-1-3 m-t-2">
-      <h4>{{$t('about.moreRetailers')}}</h4>
-    </div>
-  </div>
-    <hr>
+    <hr />
 
-    <div style="margin-top: 2rem">
+    <!-- 
+      <div style="margin-top: 2rem">
       {{$t('home.gunsaleLink')}}
       <a
         href="http://gunsale.ca/?utm_source=ammobin.ca"
@@ -71,16 +68,16 @@
         rel="noopener"
       >gunsale.ca</a>
     </div>
-    <div style="margin-top: 2rem">
-      <!-- TODO: only display this link if user ip is from USA + track clicks-->
-      <!-- not in any way connected with wikiarms, chosen b/c they have a mobile friendly website + https + display cost per round -->
-      {{$t('about.wikiamrsLink')}}
+    <div style="margin-top: 2rem">-->
+    <!-- TODO: only display this link if user ip is from USA + track clicks-->
+    <!-- not in any way connected with wikiarms, chosen b/c they have a mobile friendly website + https + display cost per round -->
+    <!-- {{$t('about.wikiamrsLink')}}
       <a
         href="https://www.wikiarms.com/?utm_source=ammobin.ca"
         target="_blank"
         rel="noopener"
       >wikiarms.com</a>
-    </div>
+    </div>-->
   </div>
 </template>
 
