d77e84ee9349c94a58bd9e2167c29aa4ecd1f10c
nexus-uw <contact@ammobin.ca>
2019-05-27 06:22:44 +0000

    more client side caching strats

--- static/custom-service-worker.js
+++ static/custom-service-worker.js
@@ -6,26 +6,46 @@ workbox.routing.registerRoute(
   workbox.strategies.staleWhileRevalidate({
     cacheName: 'api-cache',
     plugins: [
-        new workbox.expiration.Plugin({
-        // Only cache requests for 2 days
-        maxAgeSeconds: 2* 24 * 60 * 60,
+      new workbox.expiration.Plugin({
+        // Only cache requests for 1 days
+        maxAgeSeconds: 12 * 60 * 60,
         // Only cache 150 requests.
         maxEntries: 150,
       }),
-    ]
+    ],
   }),
   'GET'
-);
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+)
+
+workbox.routing.registerRoute(
+  /\.(?:js|css)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static-resources',
+  })
+)
 
 // retry track-view requests
 const bgSyncPlugin = new workbox.backgroundSync.Plugin('trackViewQueue', {
-  maxRetentionTime: 24 * 60 * 7 // Retry for max of a week
-});
+  maxRetentionTime: 24 * 60 * 7, // Retry for max of a week
+})
 
 workbox.routing.registerRoute(
-  new RegExp('/api/track-view'),
+  new RegExp('/api/'),
   workbox.strategies.networkOnly({
-    plugins: [bgSyncPlugin]
+    plugins: [bgSyncPlugin],
   }),
   'POST'
-);
+)
