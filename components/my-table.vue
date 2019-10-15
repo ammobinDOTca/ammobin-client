2fd741d206baad5d38aa98174e05e38c9eab8072
nexus-uw <contact@ammobin.ca>
2019-10-15 16:48:16 +0000

    include current query in click + view metric
    - hope to see where people are clicking from
    - ie: were filters set?

--- components/my-table.vue
+++ components/my-table.vue
@@ -274,6 +274,7 @@ export default class ItemsTable extends Vue {
   itemClicked(link) {
     const click = JSON.stringify({
       link,
+      query: this.$route.query,
       itemType: this.itemType,
       subType: this.subType,
     })
@@ -313,6 +314,7 @@ export default class ItemsTable extends Vue {
       const view = JSON.stringify({
         subType,
         brand,
+        query: this.$route.query,
         itemType: this.itemType,
       })
 
