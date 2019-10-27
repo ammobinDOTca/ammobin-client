a829aa320a60e03c13d8242edf1c640be1189343
nexus-uw <contact@ammobin.ca>
2019-10-27 20:07:58 +0000

    removed double footer

--- layouts/error.vue
+++ layouts/error.vue
@@ -5,15 +5,7 @@
       <h1 v-else>An error occurred</h1>
       <nuxt-link to="/">Home page</nuxt-link>
     </div>
-    <footer class="footer">
-      AmmoBin.ca {{ new Date().getFullYear() }}
-      <a :href="mail">Contact</a>
-      <div>
-        <a href="https://github.com/ammobindotca" target="_blank" rel="noopener">github</a> |
-        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a> |
-        <a href="https://status.ammobin.ca/" target="_blank" rel="noopener">status</a>
-      </div>
-    </footer>
+
   </div>
 </template>
 
