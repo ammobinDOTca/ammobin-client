57a4bc05e7fd1c7e87f7c6ad4bdd33be621943f2
nexus-uw <contact@ammobin.ca>
2019-10-27 18:11:24 +0000

    created basic error page
    - not sure why it took 2 years to do this...

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="container">
+      <h1 v-if="error && error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <nuxt-link to="/">Home page</nuxt-link>
+    </div>
+    <footer class="footer">
+      AmmoBin.ca {{ new Date().getFullYear() }}
+      <a :href="mail">Contact</a>
+      <div>
+        <a href="https://github.com/ammobindotca" target="_blank" rel="noopener">github</a> |
+        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a> |
+        <a href="https://status.ammobin.ca/" target="_blank" rel="noopener">status</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
