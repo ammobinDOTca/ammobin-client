bb85bd1facd6b72822c67dbe668cba099522cfb7
nexus-uw <contact@ammobin.ca>
2019-10-29 04:46:07 +0000

    set content language
    - added todo for setting lang from user's system lang

--- middleware/i18n.ts
+++ middleware/i18n.ts
@@ -1,13 +1,22 @@
-export default function({ isHMR, app, store, route, params, error, redirect }) {
+export default function ({ isHMR, app, store, route, params, error, redirect, res }) {
   // If middleware is called from hot module replacement, ignore it
   if (isHMR) {
     return
   }
   // Get locale from params. hack home page picking up lang
   const locale = route.path === '/fr/' ? 'fr' : params.LANG || 'en'
+
+  // todo: set lang to users current accept-lang header (if param not set)
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
+
   if (store.state.locales.indexOf(locale) === -1) {
     return error({ message: 'This page could not be found.', statusCode: 404 })
   }
+
+  if (process.server) {
+    res.setHeader('Content-Language', `${locale}-CA`)
+  }
+
   // Set locale
   store.commit('SET_LANG', locale)
   app.i18n.locale = store.state.locale
