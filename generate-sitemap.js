7eb4517a4d16fbeae6a664fc8d576cd89649ba83
nexus-uw <contact@ammobin.ca>
2019-11-04 01:09:27 +0000

    updated sitemap for new routes

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const head = l => l[0]
 const ws = fs.createWriteStream('./static/sitemap.txt')
-;['', 'fr/'].forEach(lang => {
-  const site = `https://ammobin.ca/${lang}`
+;['en', 'fr'].forEach(lang => {
+  const site = `https://ammobin.ca/${lang}/`
 
   ;['', 'about', 'rimfire', 'shotgun', 'centerfire', 'reloading', 'powder', 'shot', 'case', 'primer'].forEach(s =>
     ws.write(`${site}${s}\n`)
@@ -10,13 +10,13 @@ const ws = fs.createWriteStream('./static/sitemap.txt')
 
   require('ammobin-classifier/build/rimfire-calibres')
     .rimfireCalibres.map(l => head(l).toUpperCase())
-    .forEach(c => ws.write(`${site}rimfire?calibre=${encodeURIComponent(c)}&page=1\n`))
+    .forEach(c => ws.write(`${site}rimfire/${encodeURIComponent(c)}\n`))
 
   require('ammobin-classifier/build/centerfire-calibres')
     .centerFireCalibres.map(l => head(l).toUpperCase())
-    .forEach(c => ws.write(`${site}centerfire?calibre=${encodeURIComponent(c)}&page=1\n`))
+    .forEach(c => ws.write(`${site}centerfire/${encodeURIComponent(c)}\n`))
 
   require('ammobin-classifier/build/shotgun-gauges')
     .shotgunGauges.map(l => head(l).toUpperCase())
-    .forEach(c => ws.write(`${site}shotgun?calibre=${encodeURIComponent(c)}&page=1\n`))
+    .forEach(c => ws.write(`${site}shotgun/${encodeURIComponent(c)}\n`))
 })
