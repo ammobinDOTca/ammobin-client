3f7265bbe748557d693332821a3f62cd5705d5c2
Simon R <nexus-uw@users.noreply.github.com>
2021-05-28 10:31:56 -0700

    updated to proper status url

--- layouts/default.vue
+++ layouts/default.vue
@@ -62,9 +62,7 @@
       <div>
         <a href="https://github.com/ammobindotca" target="_blank" rel="noopener">github</a>
         |
-        <a href="https://www.instagram.com/ammobin.ca" target="_blank" rel="noopener">instagram</a>
-        |
-        <a href="https://status.ammobin.ca" target="_blank" rel="noopener">status</a>
+        <a href="https://stats.uptimerobot.com/XQOLXC1p2" target="_blank" rel="noopener">status</a>
       </div>
     </footer>
   </div>
