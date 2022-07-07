9bbc4976e16772b8334cc71bcf8f21e93ffa83eb
nexus-uw <contact@ammobin.ca>
2022-07-07 04:29:58 +0000

    testing explicit next version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -171,7 +171,7 @@ export default defineNuxtConfig({
     display: 'standalone',
   },
   pwa: {
-    workbox: false
+    workbox: false,
     // {
     //   importScripts: ['custom-service-worker.js'],
     //   globIgnores: ['sw.js', '**/workbox*.js'],
